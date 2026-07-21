830c214f4338a8f8c069debf60de033c1ad0ea3a
Komal Rajvi <154300498+Komalrajvi@users.noreply.github.com>
2026-07-21 23:29:34 +0530

    Time: 13 ms (31.63%), Space: 52.5 MB (85.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,28 +1,28 @@
-class Solution {
-  public int majorityElement(int[] num) {
-    Arrays.sort(num);
-    int n = num.length;
-    return num[n/2];
-  }
-}
+// class Solution {
+//   public int majorityElement(int[] num) {
+//     Arrays.sort(num);
+//     int n = num.length;
+//     return num[n/2];
+//   }
+// }
 
 //hashmap::
-//class Solution {
-//     public int majorityElement(int[] nums) {
-//         int n = nums.length;
-//         Map<Integer, Integer> map = new HashMap<>();
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
         
-//         for (int i = 0; i < n; i++) {
-//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-//         }
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
         
-//         n = n / 2;
-//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-//             if (entry.getValue() > n) {
-//                 return entry.getKey();
-//             }
-//         }
+        n = n / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
         
-//         return 0;
-//     }
-// }
+        return 0;
+    }
+}
